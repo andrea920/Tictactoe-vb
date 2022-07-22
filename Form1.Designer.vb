748ed9fbab0c3509be66e2df6d694a63f0f64ec9
Ma. Andrea G. Perido <andreamaperido@gmail.com>
2022-07-23 00:29:56 +0800

    initial with buttons

---
+++ Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnA = New System.Windows.Forms.Button()
+        Me.btnAA = New System.Windows.Forms.Button()
+        Me.btnAAA = New System.Windows.Forms.Button()
+        Me.btnB = New System.Windows.Forms.Button()
+        Me.btnBB = New System.Windows.Forms.Button()
+        Me.btnBBB = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.btnCC = New System.Windows.Forms.Button()
+        Me.btnCCC = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnA
+        '
+        Me.btnA.Location = New System.Drawing.Point(23, 12)
+        Me.btnA.Name = "btnA"
+        Me.btnA.Size = New System.Drawing.Size(94, 83)
+        Me.btnA.TabIndex = 0
+        Me.btnA.UseVisualStyleBackColor = True
+        '
+        'btnAA
+        '
+        Me.btnAA.Location = New System.Drawing.Point(123, 12)
+        Me.btnAA.Name = "btnAA"
+        Me.btnAA.Size = New System.Drawing.Size(94, 83)
+        Me.btnAA.TabIndex = 0
+        Me.btnAA.UseVisualStyleBackColor = True
+        '
+        'btnAAA
+        '
+        Me.btnAAA.Location = New System.Drawing.Point(223, 12)
+        Me.btnAAA.Name = "btnAAA"
+        Me.btnAAA.Size = New System.Drawing.Size(94, 83)
+        Me.btnAAA.TabIndex = 0
+        Me.btnAAA.UseVisualStyleBackColor = True
+        '
+        'btnB
+        '
+        Me.btnB.Location = New System.Drawing.Point(23, 101)
+        Me.btnB.Name = "btnB"
+        Me.btnB.Size = New System.Drawing.Size(94, 83)
+        Me.btnB.TabIndex = 0
+        Me.btnB.UseVisualStyleBackColor = True
+        '
+        'btnBB
+        '
+        Me.btnBB.Location = New System.Drawing.Point(123, 101)
+        Me.btnBB.Name = "btnBB"
+        Me.btnBB.Size = New System.Drawing.Size(94, 83)
+        Me.btnBB.TabIndex = 0
+        Me.btnBB.UseVisualStyleBackColor = True
+        '
+        'btnBBB
+        '
+        Me.btnBBB.Location = New System.Drawing.Point(223, 101)
+        Me.btnBBB.Name = "btnBBB"
+        Me.btnBBB.Size = New System.Drawing.Size(94, 83)
+        Me.btnBBB.TabIndex = 0
+        Me.btnBBB.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.Location = New System.Drawing.Point(23, 190)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(94, 83)
+        Me.btnC.TabIndex = 0
+        Me.btnC.UseVisualStyleBackColor = True
+        '
+        'btnCC
+        '
+        Me.btnCC.Location = New System.Drawing.Point(123, 190)
+        Me.btnCC.Name = "btnCC"
+        Me.btnCC.Size = New System.Drawing.Size(94, 83)
+        Me.btnCC.TabIndex = 0
+        Me.btnCC.UseVisualStyleBackColor = True
+        '
+        'btnCCC
+        '
+        Me.btnCCC.Location = New System.Drawing.Point(223, 190)
+        Me.btnCCC.Name = "btnCCC"
+        Me.btnCCC.Size = New System.Drawing.Size(94, 83)
+        Me.btnCCC.TabIndex = 0
+        Me.btnCCC.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(45, 293)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "New Game"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(202, 293)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Clear All"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(338, 344)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnCCC)
+        Me.Controls.Add(Me.btnBBB)
+        Me.Controls.Add(Me.btnAAA)
+        Me.Controls.Add(Me.btnCC)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btnBB)
+        Me.Controls.Add(Me.btnB)
+        Me.Controls.Add(Me.btnAA)
+        Me.Controls.Add(Me.btnA)
+        Me.Name = "Form1"
+        Me.Text = "My TicTacToe"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnA As Button
+    Friend WithEvents btnAA As Button
+    Friend WithEvents btnAAA As Button
+    Friend WithEvents btnB As Button
+    Friend WithEvents btnBB As Button
+    Friend WithEvents btnBBB As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnCC As Button
+    Friend WithEvents btnCCC As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Dim Tic As Char = "X"
+    Private Sub btnA_Click(sender As Object, e As EventArgs) Handles btnA.Click
+        If btnA.Text = "" Then
+            btnA.Text = Tic
+
+            If Tic = "X" Then
+                Tic = "O"
+            Else
+                Tic = "X"
+            End If
+        End If
+    End Sub
+
+    Private Sub btnAA_Click(sender As Object, e As EventArgs) Handles btnAA.Click
+        If btnAA.Text = "" Then
+            btnAA.Text = Tic
+
+            If Tic = "X" Then
+                Tic = "O"
+            Else
+                Tic = "X"
+            End If
+        End If
+    End Sub
+
+    Private Sub btnAAA_Click(sender As Object, e As EventArgs) Handles btnAAA.Click
+        If btnAAA.Text = "" Then
+            btnAAA.Text = Tic
+
+            If Tic = "X" Then
+                Tic = "O"
+            Else
+                Tic = "X"
+            End If
+        End If
+    End Sub
+End Class
